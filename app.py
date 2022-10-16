3c0d4ee87947b8a5018db5d74f04779585f2feb1
gierot <gierot@hotmail.com>
2022-10-16 16:43:37 -0300

    renderização da pagina

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import flaskr
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
-app =flaskr.create_app()
 
-app.run("0.0.0.0", 5000)
+@app.route("/")
+def home():
+    return render_template('index.html')
